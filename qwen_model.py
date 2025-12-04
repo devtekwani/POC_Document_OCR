a1e47c2498a8ecd0698cfdb574cd1cea8454ba84
Dev Tekwani <dev.tekwani@programmers.io>
2025-12-04 14:02:57 +0530

    update qwen model 32B params

--- qwen_model.py
+++ qwen_model.py
@@ -6,7 +6,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    torch_dtype="auto",
+    dtype="auto",
     device_map="auto"
 )
 
